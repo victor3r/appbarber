e608b2ce6c2171c05a34efe09430b38b1d559390
Victor Rabelo <victorabelo1697@gmail.com>
2020-11-07 21:17:47 -0300

    :sparkles: configuring app routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { View } from 'react-native';
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
 
-// import { Container } from './styles';
+import List from '../pages/List';
+import Profile from '../pages/Profile';
 
-const Routes: React.FC = () => {
-  return <View />;
-};
+const App = createStackNavigator();
+
+const Routes: React.FC = () => (
+  <App.Navigator screenOptions={{ headerShown: false }}>
+    <App.Screen name="List" component={List} />
+    <App.Screen name="Profile" component={Profile} />
+  </App.Navigator>
+);
 
 export default Routes;
